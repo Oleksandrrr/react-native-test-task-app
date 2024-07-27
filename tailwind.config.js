440cd2e0ee4cd8ddce76396bd10dae3937364e6e
“user” <oleksandr.yurchenko@extelasoft.com>
2024-07-27 16:12:03 +0300

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        abel: ['Abel-Regular', 'sans-serif'],
+        sfProDisplay: ['SFProDisplay-Regular', 'sans-serif'],
+      },
+    },
+  },
   plugins: [],
 }
